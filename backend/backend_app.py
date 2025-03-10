7f19a178c3f5bb76b0aabb0aae8261e7bfff0f6c
Gabo <no@email>
2025-03-10 16:46:58 +0100

    UPDATED docstrings

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,3 +1,7 @@
+"""
+    Contains functions and routes for all API functionalities
+"""
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
@@ -21,12 +25,17 @@ app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
-    {"id": 3, "title": "Zapatillas", "content": "Zapatos, zapatillas"},
-    {"id": 4, "title": "Sombreros", "content": "Gorros y sombreros"},
+    {"id": 3, "title": "Third post", "content": "This is the third post"},
+    {"id": 4, "title": "Fourth post", "content": "This is the fourth post"},
 ]
 
 
 def generate_new_id(list_posts):
+    """
+        Generates an id.
+        If a number between 1 and the maximum id number is not used, it takes this number as the new id.
+        If there is no free number, it adds a new id equal to the greater id number +1.
+    """
     id_list = []
     for post in list_posts:
         id_list.append(post["id"])
@@ -47,8 +56,21 @@ def check_new_post(post):
         return jsonify("Missing Field 'content'"), 400
 
 
+def fetch_post_by_id(post_id):
+    """ Returns the dictionary of the post containing the given id (post_id) """
+    for post in POSTS:
+        if post['id'] == post_id:
+            return post
+    return False
+
+
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    """
+        Gets and show all post.
+        Allows to add a new post.
+        Allows to sort by title or content in asc or desc direction
+    """
     if request.method == 'POST':
         new_post = request.get_json()
         if check_new_post(new_post):
@@ -70,32 +92,32 @@ def get_posts():
             return jsonify("direction key have to be asc or desc")
         try:
             sorted_posts = sorted(POSTS, key=lambda x: x[sort], reverse=direction)
-        except KeyError as e:
+        except KeyError:
             return jsonify("sort key have to be title or content")
         return jsonify(sorted_posts)
     return jsonify(POSTS)
 
 
-def fetch_post_by_id(post_id):
-    """ Returns the dictionary of the post containing the given id (post_id) """
-    for post in POSTS:
-        if post['id'] == post_id:
-            return post
-    return False
-
-
 @app.route('/api/posts/<int:post_id>', methods=["DELETE"])
 def delete(post_id):
+    """
+        Gets a post ID in the url request.
+        Deletes the post with the given ID
+    """
     if request.method == 'DELETE':
         post = fetch_post_by_id(post_id)
         if not post:
             return jsonify("Post was not found"), 404
         POSTS.remove(post)
-        return jsonify(f'"message": "Post with id {post_id} has been deleted successfully."'), 200
+        return jsonify(f'"The post with id {post_id} has been deleted successfully."'), 200
 
 
 @app.route('/api/posts/<int:post_id>', methods=["PUT"])
 def update(post_id):
+    """
+        Gets the ID of a post in the url request.
+        Through the PUT method updates the post by sending title and/or content as parameters, with the new data.
+    """
     if request.method == 'PUT':
         update_post = request.get_json()
         post = fetch_post_by_id(post_id)
@@ -110,10 +132,11 @@ def update(post_id):
         POSTS[id_post_in_list] = post
         return jsonify(post), 200
 
+
 @app.route('/api/posts/search', methods=["GET"])
 def search():
     """ GET Requests for 'title' and 'content'
-        If gets a variable iterate in the list searching for the value
+        If it gets a variable iterate in the list searching for the value
     """
     if request.method == 'GET':
         list_found = []
@@ -129,11 +152,6 @@ def search():
                         list_found.append(post)
         return list_found
 
-
-@app.errorhandler(400)
-def bad_request():
-    """ Render '404.html' """
-    return jsonify("Field missing"), 400
 
 
 if __name__ == '__main__':
